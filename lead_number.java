2e582cda65eeb6fe3d42a626b762444df81e1429
Sujoy Muhuri <sujoymuhuri9@gmail.com>
2024-08-03 00:20:12 +0530

    Add files via upload

---
+++ lead_number.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class lead_number 
+{
+    public static void main(String[] args)throws IOException. // Main method
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int n = sc.nextInt();  // Get the input
+
+        int sume=0 , sumo=0,rem;  // Declare variable
+
+        while (n>0) {
+            rem=n%10;
+            if (rem%2==0) {
+                sume+=rem;
+            }
+            else {
+                sumo+=rem;
+            }
+            n=n/10;  // Remove the last digit
+        }
+        System.out.println("Sum of even digits : "+sume+"\n"+"Sum of odd digits: "+sumo);  // Show output
+    }
+}
